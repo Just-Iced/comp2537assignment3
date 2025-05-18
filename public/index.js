31bc06efa5990ae34a3dbc7a65539ecca64a4514
Just-Iced <plunkettjustice@gmail.com>
2025-05-18 00:14:40 -0700

    Styled the pokemon correctly with rows

--- public/index.js
+++ public/index.js
@@ -1,15 +1,18 @@
 const pokemonList = document.getElementById("pokemonList");
 const pokemonTemplate = document.getElementById("pokemonTemplate");
+const pokemonGrid = document.getElementById("pokemonGrid");
 
-async function loadPokemon() {
+async function loadPokemon(size = 16) {
+    let rows = Math.sqrt(size);
     let ids = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < size; i++) {
         let id = Math.floor(Math.random() * 1000) + 1;
         while (ids.includes(id)) {
             id = Math.floor(Math.random() * 1000) + 1;
         }
         ids.push(id);
     }
+    curId = 0;
     for (let id of ids) {
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${id}&limit=10`);
         let jsonObj = await response.json();
@@ -19,12 +22,35 @@ async function loadPokemon() {
         for (let i = 0; i < 2; i++) {
             let newPokemon = pokemonTemplate.content.cloneNode(true);
             newPokemon.querySelector(".card-img-top").src = thisPokemon.sprites.other['official-artwork'].front_default;
+            newPokemon.id = `pokemon-${curId}`;
             pokemonList.appendChild(newPokemon);
+            curId++;
         }
     }
     for (let i = pokemonList.children.length; i >= 0; i--) {
         pokemonList.appendChild(pokemonList.children[Math.random() * i | 0]);
     }
+    for (let i = 0; i < rows; i++) {
+        let newRow = document.createElement("div");
+        newRow.classList.add("row");
+        pokemonGrid.appendChild(newRow);
+        for (let t = 0; t < rows; t++) {
+            newRow.appendChild(pokemonList.children[(i * rows) + t]);
+        }
+    }
+    pokemonList.remove();
+    let cardStyle = document.createElement("style");
+    cardStyle.innerHTML = `
+        .card {
+            min-width: ${Math.floor(90 / rows)}% !important;
+            max-width: ${Math.floor(90 / rows)}% !important;
+            width: ${Math.floor(90 / rows)}% !important;
+            min-height: ${Math.floor(600 / rows)}px !important;
+            max-height: ${Math.floor(600 / rows)}px !important;
+            height: ${Math.floor(600 / rows)}% !important;
+        }
+            `;
+    document.head.appendChild(cardStyle);
 }
 
 function setup() {
@@ -32,8 +58,12 @@ function setup() {
     let secondCard;
     document.querySelectorAll(".card").forEach(card => {
         card.addEventListener("click", function click(e) {
-            e.preventDefault();
+            if (!card.classList.contains("flip")) {
+                return;
+            }
             card.classList.toggle("flip");
+            e.preventDefault();
+            
             if (!firstCard) {
                 firstCard = card.querySelector("img");
             } else {
